67f4b381431fbe87faed53ea9321953d319b71df
SARAH AHMAD HUDAIB <SARAHHUDAIB@YAHOO.COM>
2022-07-31 11:40:25 +0300

    navbar

--- pages/components/NavBar.jsx
+++ pages/components/NavBar.jsx
@@ -1,49 +1,44 @@
+function Navbar() {
+  return (
+    <nav className="flex items-center justify-between h-16 gap-2 p-2 m-0 text-white list-none bg-violet-500 dark:md:hover:bg-violet-400">
+      <a href="/" className="flex text-2xl font-semibold tracking-wide drop-shadow-md">
+        AMARO & SOSO Co.
+      </a>
+      <ul className="flex items-stretch gap-4 p-2 m-0">
+        <li >
+          <a href="/product"  className="flex items-stretch hover:bg-violet-700">Our Products</a>
+        </li>
 
+        <li>
+          <a href="/about" className="flex items-stretch hover:bg-violet-700">About</a>
+        </li>
 
-const NavBar = () => {
-
-
-    return (
-        <nav className="flex h-10 gap-3 bg-red-900">
-
-            <ul className="flex gap-3">
-                <li className="left-1">My Website</li>
-                < div className="absolute top-0 right-0">
-                    <ul className="flex">
-                        <li className="mr-3 ">Home</li>
-                        <li className="ml-10 ">Contact Us</li>
-                    </ul>
-                </div >
-
-            </ul >
-        </nav >
-    )
-
-
+        <li>
+          <a href="/contact" className="flex items-stretch hover:bg-violet-700">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
+export default Navbar;
 
-export default NavBar
+// const NavBar = () => {
 
+//     return (
+//         <nav className="flex h-10 gap-3 bg-red-900">
 
+//             <ul className="flex gap-3">
+//                 <li className="left-1">My Website</li>
+//                 < div className="absolute top-0 right-0">
+//                     <ul className="flex">
+//                         <li className="mr-3 ">Home</li>
+//                         <li className="ml-10 ">Contact Us</li>
+//                     </ul>
+//                 </div >
 
-// () => {
-//     <div className="navbar ">?!?!</div>
-
-//     {
-
-
-
-
-//     }
-// }
-
-
-
-// {/* <div> {hi.map((ele) => (<p>{ele}</p>))}</div>
-//         {console.log("im cons", hi)} */}
-
-// {
-//     hi.map((item, index) => (<p key={index}> {item} </p>)
+//             </ul >
+//         </nav >
 //     )
+
 // }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,43 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+
 import NavBar from './components/NavBar'
 import Body from './components/Body'
+import About from './components/About'
+import Contact from './components/Contact'
+import Product from './components/Product'
 
-const Home = () => {
+function Home () {
+  // let comp
+  
+  // switch (window.location.pathname) {
+  //   case '/':
+  //     comp = Home
+  //     break
+  //   case '/about':
+  //     comp = About
+  //     break
+  //   case '/contact':
+  //     comp = Contact
+  //     break
+  //   case '/product':
+  //     comp = Product
+  //     break
+  // }
 
   return (
     <>
       <NavBar />
       <Body />
+      {/* <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </div> */}
     </>
   )
 
